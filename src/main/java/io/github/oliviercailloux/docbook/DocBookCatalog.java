4631322abb549174a25175f46a4ca818ea2f244b
Olivier Cailloux <olivier.cailloux@gmail.com>
2025-04-21 20:47:52 +0200

    With static catalog

--- src/main/java/io/github/oliviercailloux/docbook/DocBookCatalog.java
+++ src/main/java/io/github/oliviercailloux/docbook/DocBookCatalog.java
@@ -2,8 +2,16 @@ package io.github.oliviercailloux.docbook;
 
 import com.google.common.io.Resources;
 import java.net.URI;
+import javax.xml.catalog.Catalog;
+import javax.xml.catalog.CatalogFeatures;
+import javax.xml.catalog.CatalogFeatures.Feature;
+import javax.xml.catalog.CatalogManager;
+import javax.xml.catalog.CatalogResolver;
 
 public class DocBookCatalog {
   public static URI URI =
       java.net.URI.create(Resources.getResource(DocBookCatalog.class, "catalog.xml").toString());
+  public static Catalog CATALOG = CatalogManager
+      .catalog(CatalogFeatures.builder().with(Feature.RESOLVE, "continue").build(), URI);
+  public static CatalogResolver RESOLVER = CatalogManager.catalogResolver(CATALOG);
 }
