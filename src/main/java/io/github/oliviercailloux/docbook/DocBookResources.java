48495b880bdb2386d5e681cb37435fefef8c7bad
Olivier Cailloux <olivier.cailloux@gmail.com>
2025-09-07 00:02:17 +0200

    xslTNG

--- src/main/java/io/github/oliviercailloux/docbook/DocBookResources.java
+++ src/main/java/io/github/oliviercailloux/docbook/DocBookResources.java
@@ -1,5 +1,6 @@
 package io.github.oliviercailloux.docbook;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.io.Resources;
 import java.net.URI;
 import javax.xml.catalog.Catalog;
@@ -8,6 +9,10 @@ import javax.xml.catalog.CatalogFeatures.Feature;
 import javax.xml.catalog.CatalogManager;
 import javax.xml.catalog.CatalogResolver;
 import org.docbook.schemas.docbook.DocBook;
+import org.docbook.xsltng.Main;
+import org.docbook.xsltng.XslTNG;
+import org.xmlresolver.XMLResolver;
+import org.xmlresolver.XMLResolverConfiguration;
 
 /**
  * Static resources found in the class path and public ids for use with a catalog.
@@ -28,11 +33,16 @@ public class DocBookResources {
       java.net.URI.create("http://cdn.docbook.org/release/xsl/1.79.2/html/docbook.xsl");
 
   /** The URI of the DocBook XSLT 1 Catalog in the class path */
-  public static URI CATALOG_URI =
+  public static URI CATALOG_1_URI =
       java.net.URI.create(Resources.getResource(DocBookResources.class, "catalog.xml").toString());
+  /** The URI of the DocBook XSLT 3 (xslTNG) Catalog in the class path */
+  public static URI CATALOG_3_URI =
+      java.net.URI.create(Resources.getResource(XslTNG.class, "catalog.xml").toString());
   /** The DocBook XSLT 1 Catalog, read from the class path */
-  public static Catalog CATALOG = CatalogManager
-      .catalog(CatalogFeatures.builder().with(Feature.RESOLVE, "continue").build(), CATALOG_URI);
-  /** The DocBook XSLT 1 Catalog resolver, read from the class path */
-  public static CatalogResolver RESOLVER = CatalogManager.catalogResolver(CATALOG);
+  public static Catalog CATALOG_1 = CatalogManager
+      .catalog(CatalogFeatures.builder().with(Feature.RESOLVE, "continue").build(), CATALOG_1_URI);
+  /** The DocBook XSLT 1 Catalog resolver, reading from the class path */
+  public static CatalogResolver RESOLVER_1 = CatalogManager.catalogResolver(CATALOG_1);
+  /** The DocBook XSLT XML resolver, reading from the XSLT 1 and 3 catalogs from the class path */
+  public static XMLResolver XML_RESOLVER = new XMLResolver(new XMLResolverConfiguration(ImmutableList.of(DocBookResources.CATALOG_1_URI.toString(), DocBookResources.CATALOG_3_URI.toString())));
 }
--- src/test/java/io/github/oliviercailloux/docbook/MyTests.java
+++ src/test/java/io/github/oliviercailloux/docbook/MyTests.java
@@ -1,10 +1,18 @@
 package io.github.oliviercailloux.docbook;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.google.common.collect.ImmutableList;
+import javax.xml.transform.URIResolver;
+import javax.xml.transform.sax.SAXSource;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.xmlresolver.XMLResolver;
+import org.xmlresolver.XMLResolverConfiguration;
 
 public class MyTests {
   @SuppressWarnings("unused")
@@ -15,8 +23,85 @@ public class MyTests {
     assertNotNull(DocBookResources.XSD_5_0_URI);
     assertNotNull(DocBookResources.RNG_5_1_URI);
     assertNotNull(MyTests.class.getResource("/io/github/oliviercailloux/docbook/catalog.xml"));
-    assertNotNull(DocBookResources.CATALOG_URI);
+    assertNotNull(DocBookResources.CATALOG_1_URI);
     assertNotNull(MyTests.class.getResource("catalog.xml"));
     assertNotNull(MyTests.class.getResource("fo/fo.xsl"));
+    assertTrue(DocBookResources.CATALOG_3_URI.toString()
+        .matches("jar:file:/.*/docbook-xslTNG-2.6.0.jar!/org/docbook/xsltng/catalog.xml"));
+  }
+
+  @Test
+  void testResolver1() throws Exception {
+    assertTrue(DocBookResources.RESOLVER_1
+        .resolve("http://cdn.docbook.org/release/xsl/1.79.2/fo/docbook.xsl", null).getSystemId()
+        .matches("file:/.*/fo/docbook.xsl"));
+    assertTrue(DocBookResources.RESOLVER_1
+        .resolve("http://cdn.docbook.org/release/xsl/1.79.2/anythingreally", null).getSystemId()
+        .matches("file:/.*/anythingreally"));
+  }
+
+  @Test
+  void testXmlResolver0() throws Exception {
+    XMLResolverConfiguration config = new XMLResolverConfiguration();
+    URIResolver uriResolver = new XMLResolver(config).getURIResolver();
+
+    assertEquals("https://cdn.docbook.org/release/xsl/1.79.2/fo/docbook.xsl", uriResolver
+        .resolve("http://cdn.docbook.org/release/xsl/1.79.2/fo/docbook.xsl", null).getSystemId());
+    assertNull(
+        uriResolver.resolve("http://cdn.docbook.org/release/xsl/1.79.2/anythingreally", null));
+  }
+
+  @Test
+  void testXmlResolver1() throws Exception {
+    XMLResolverConfiguration config =
+        new XMLResolverConfiguration(ImmutableList.of(DocBookResources.CATALOG_1_URI.toString()));
+    URIResolver uriResolver = new XMLResolver(config).getURIResolver();
+
+    assertTrue(uriResolver.resolve("http://cdn.docbook.org/release/xsl/1.79.2/fo/docbook.xsl", null)
+        .getSystemId().matches("file:/.*/fo/docbook.xsl"));
+    assertNull(
+        uriResolver.resolve("http://cdn.docbook.org/release/xsl/1.79.2/anythingreally", null));
+  }
+
+  @Test
+  void testXmlResolver3() throws Exception {
+    XMLResolverConfiguration config =
+        new XMLResolverConfiguration(ImmutableList.of(DocBookResources.CATALOG_3_URI.toString()));
+    URIResolver uriResolver = new XMLResolver(config).getURIResolver();
+
+    assertEquals("https://cdn.docbook.org/release/xsl/1.79.2/fo/docbook.xsl", uriResolver
+        .resolve("http://cdn.docbook.org/release/xsl/1.79.2/fo/docbook.xsl", null).getSystemId());
+    assertNull(uriResolver
+        .resolve("http://cdn.docbook.org/release/xsl/1.79.2/anythingreally", null));
+
+    SAXSource resolved = (SAXSource) uriResolver
+        .resolve("https://cdn.docbook.org/release/xsltng/2.6.0/xslt/docbook.xsl", null);
+    assertEquals("https://cdn.docbook.org/release/xsltng/2.6.0/xslt/docbook.xsl",
+        resolved.getSystemId());
+    assertNull(resolved.getXMLReader());
+    assertNull(resolved.getInputSource().getPublicId());
+    assertNull(resolved.getInputSource().getCharacterStream());
+    assertEquals(21052, resolved.getInputSource().getByteStream().readAllBytes().length);
+  }
+
+  @Test
+  void testXmlResolver1And3() throws Exception {
+    URIResolver uriResolver = DocBookResources.XML_RESOLVER.getURIResolver();
+    assertNull(uriResolver
+        .resolve("http://cdn.docbook.org/release/xsl/1.79.2/anythingreally", null));
+
+    assertTrue(uriResolver.resolve("http://cdn.docbook.org/release/xsl/1.79.2/fo/docbook.xsl", null)
+        .getSystemId().matches("file:/.*/fo/docbook.xsl"));
+    assertNull(
+        uriResolver.resolve("http://cdn.docbook.org/release/xsl/1.79.2/anythingreally", null));
+
+    SAXSource resolved = (SAXSource) uriResolver
+        .resolve("https://cdn.docbook.org/release/xsltng/2.6.0/xslt/docbook.xsl", null);
+    assertEquals("https://cdn.docbook.org/release/xsltng/2.6.0/xslt/docbook.xsl",
+        resolved.getSystemId());
+    assertNull(resolved.getXMLReader());
+    assertNull(resolved.getInputSource().getPublicId());
+    assertNull(resolved.getInputSource().getCharacterStream());
+    assertEquals(21052, resolved.getInputSource().getByteStream().readAllBytes().length);
   }
 }
